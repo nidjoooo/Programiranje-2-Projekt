8cefc63d1fb4cb2fd865117873a4a0b5c00df56c
nidjoooo <babysonbusy@gmail.com>
2025-05-26 09:44:05 +0200

    Update search.h

--- search.h
+++ search.h
@@ -1,2 +1,16 @@
-#pragma once
-void searchByUser(void);
+#ifndef SEARCH_H
+#define SEARCH_H
+
+#include "header.h"
+
+void searchMenu();
+
+void searchByUser();
+void searchByMuscleGroup();
+void searchByDate();
+
+int compareWorkoutsByUser(const void* a, const void* b);
+int compareWorkoutsByMuscleGroup(const void* a, const void* b);
+int compareWorkoutsByDate(const void* a, const void* b);
+
+#endif
