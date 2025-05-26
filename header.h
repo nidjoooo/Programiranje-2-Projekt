ef6d43048dea9b55e1dbbcfd8909a5b2e6495f1a
nidjoooo <babysonbusy@gmail.com>
2025-05-26 09:43:13 +0200

    Update header.h

--- header.h
+++ header.h
@@ -1,29 +1,55 @@
+#ifndef HEADER_H
+#define HEADER_H
+
 #define _CRT_SECURE_NO_WARNINGS
 
-#pragma once
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
-typedef struct workout {
-    char date[21];
-    char muscleGroup[51];
-    char exerciseName[51];
-    int sets;
-    int reps;
-    float weight;
-    float duration[21];
-    float progress;
-} WORKOUT;
+#define MAX_NAME_LENGTH 100
+#define MAX_EXERCISE_LENGTH 100
 
-enum MainMenu {
-    ADD_WORKOUT = 1,
-    DELETE_WORKOUT = 2,
-    DELETE_FILE = 3,
-    EXIT_PROGRAM = 4
-};
+typedef struct {
+	char date[20];
+	char muscleGroup[50];
+	char exercise[MAX_EXERCISE_LENGTH];
+	int sets;
+	int reps;
+	float weight;
+	int duration;
+	char user[MAX_NAME_LENGTH];
+} Workout;
 
-void addWorkout(void);
-void deleteWorkout(void);
-void deleteFile(void);
-int exitProgram(void);
+typedef enum {
+	MENU_ADD = 1,
+	MENU_LIST,
+	MENU_UPDATE,
+	MENU_DELETE,
+	MENU_SEARCH,
+	MENU_EXIT
+} MenuOptions;
+
+typedef enum {
+	SEARCH_BY_USER = 1,
+	SEARCH_BY_MUSCLE_GROUP,
+	SEARCH_BY_DATE
+} SearchOptions;
+
+extern Workout* workouts;
+extern int workoutCount;
+
+void loadFromFile();
+void saveToFile();
+void freeWorkouts();
+
+void addWorkout();
+void listWorkouts();
+void updateWorkout();
+void deleteWorkout();
+
+void showMenu();
+
+#endif
 
