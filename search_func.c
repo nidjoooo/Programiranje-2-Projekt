8bb7ed3468000f0c389c94c8ba839cec04c47dc6
nidjoooo <babysonbusy@gmail.com>
2025-06-01 18:12:41 +0200

    Update search_func.c

--- search_func.c
+++ search_func.c
@@ -1,117 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include "search.h"
 
-extern Workout* workouts;
-extern int workoutCount;
+void searchMenu() {
+    int choice;
+    printf("\n=== IronFlow: Workout Search ===\n");
+    printf("1. By user\n");
+    printf("2. By muscle group\n");
+    printf("3. By date\n");
+    printf("Choice: ");
+    scanf("%d", &choice);
 
-static void printWorkout(int index, const Workout* w) {
-	printf("[%d] %s | %s | %s | %s | %d x %d x %.2fkg | %dmin\n",
-		index + 1, w->user, w->date, w->muscleGroup,
-		w->exercise, w->sets, w->reps, w->weight, w->duration);
+    switch (choice) {
+    case SEARCH_BY_USER:
+        searchByUser();
+        break;
+    case SEARCH_BY_MUSCLE_GROUP:
+        searchByMuscleGroup();
+        break;
+    case SEARCH_BY_DATE:
+        searchByDate();
+        break;
+    default:
+        printf("Invalid selection.\n");
+    }
 }
 
 void searchByUser() {
-	char name[MAX_NAME_LENGTH];
-	printf("Enter user name to search: ");
-	if (!fgets(name, MAX_NAME_LENGTH, stdin)) return;
-	strtok(name, "\n");
+    char firstName[51];
+    printf("Enter user's first name to search: ");
+    scanf("%50s", firstName);
 
-	qsort(workouts, workoutCount, sizeof(Workout), compareWorkoutsByUser);
-
-	Workout key = { 0 };
-	strncpy(key.user, name, MAX_NAME_LENGTH - 1);
-	Workout* found = (Workout*)bsearch(&key, workouts, workoutCount, sizeof(Workout), compareWorkoutsByUser);
-
-	int foundAny = 0;
-	if (found) {
-		int i = (int)(found - workouts);
-		for (; i < workoutCount && strcmp(workouts[i].user, name) == 0; i++) {
-			printWorkout(i, &workouts[i]);
-			foundAny = 1;
-		}
-	}
-
-	if (!foundAny) printf("No workouts found for user: %s\n", name);
+    for (int i = 0; i < workoutCount; i++) {
+        if (strcmp(workouts[i].name, firstName) == 0) {
+            printf("User: %s %s, Date: %s\n", workouts[i].name, workouts[i].surname, workouts[i].date);
+        }
+    }
 }
 
 void searchByMuscleGroup() {
-	char muscle[50];
-	printf("Enter muscle group to search: ");
-	if (!fgets(muscle, sizeof(muscle), stdin)) return;
-	strtok(muscle, "\n");
+    char group[51];
+    printf("Enter muscle group to search: ");
+    scanf("%50s", group);
 
-	qsort(workouts, workoutCount, sizeof(Workout), compareWorkoutsByMuscleGroup);
-
-	Workout key = { 0 };
-	strncpy(key.muscleGroup, muscle, sizeof(key.muscleGroup) - 1);
-	Workout* found = (Workout*)bsearch(&key, workouts, workoutCount, sizeof(Workout), compareWorkoutsByMuscleGroup);
-
-	int foundAny = 0;
-	if (found) {
-		int i = (int)(found - workouts);
-		for (; i < workoutCount && strcmp(workouts[i].muscleGroup, muscle) == 0; i++) {
-			printWorkout(i, &workouts[i]);
-			foundAny = 1;
-		}
-	}
-
-	if (!foundAny) printf("No workouts found for muscle group: %s\n", muscle);
+    for (int i = 0; i < workoutCount; i++) {
+        if (strcmp(workouts[i].muscleGroup, group) == 0) {
+            printf("Muscle group: %s, User: %s %s\n", group, workouts[i].name, workouts[i].surname);
+        }
+    }
 }
 
 void searchByDate() {
-	char date[20];
-	printf("Enter date to search: ");
-	if (!fgets(date, sizeof(date), stdin)) return;
-	strtok(date, "\n");
+    char date[11];
+    printf("Enter workout date (dd.mm.yyyy): ");
+    scanf("%10s", date);
 
-	qsort(workouts, workoutCount, sizeof(Workout), compareWorkoutsByDate);
-
-	Workout key = { 0 };
-	strncpy(key.date, date, sizeof(key.date) - 1);
-	Workout* found = (Workout*)bsearch(&key, workouts, workoutCount, sizeof(Workout), compareWorkoutsByDate);
-
-	int foundAny = 0;
-	if (found) {
-		int i = (int)(found - workouts);
-		for (; i < workoutCount && strcmp(workouts[i].date, date) == 0; i++) {
-			printWorkout(i, &workouts[i]);
-			foundAny = 1;
-		}
-	}
-
-	if (!foundAny) printf("No workouts found for date: %s\n", date);
+    for (int i = 0; i < workoutCount; i++) {
+        if (strcmp(workouts[i].date, date) == 0) {
+            printf("Date: %s, User: %s %s\n", date, workouts[i].name, workouts[i].surname);
+        }
+    }
 }
 
-void searchMenu() {
-	while (1) {
-		printf("\nSearch Menu:\n");
-		printf("1. Search by User\n");
-		printf("2. Search by Muscle Group\n");
-		printf("3. Search by Date\n");
-		printf("4. Return to Main Menu\n");
-		printf("Choose option: ");
 
-		int option;
-		if (scanf("%d", &option) != 1) {
-			while (getchar() != '\n');
-			printf("Invalid input\n");
-			continue;
-		}
-		while (getchar() != '\n');
-
-		switch (option) {
-		case SEARCH_BY_USER:
-			searchByUser();
-			break;
-		case SEARCH_BY_MUSCLE_GROUP:
-			searchByMuscleGroup();
-			break;
-		case SEARCH_BY_DATE:
-			searchByDate();
-			break;
-		case 4:
-			return;
-		default:
-			printf("Invalid option\n");
-		}
-	}
-}
