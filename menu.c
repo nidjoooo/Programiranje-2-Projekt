cd369d5f205bd6be901c188f04f954c503271be8
nidjoooo <babysonbusy@gmail.com>
2025-06-01 18:12:07 +0200

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,53 +1,46 @@
+#define _CRT_SECURE_NO_WARNINGS
+
 #include "header.h"
 #include "search.h"
 
-extern Workout* workouts;
-extern int workoutCount;
+void showMainMenu() {
+    int choice;
+    do {
+        printf("\n==== IronFlow Workout Tracker ====\n");
+        printf("1. Add workout\n");
+        printf("2. List all workouts\n");
+        printf("3. Update a workout\n");
+        printf("4. Delete a workout\n");
+        printf("5. Search workouts\n");
+        printf("6. Exit\n");
+        printf("Choice: ");
+        scanf("%d", &choice);
 
-void showMenu() {
-	while (1) {
-		printf("\nMain Menu:\n");
-		printf("1. Add Workout\n");
-		printf("2. List Workouts\n");
-		printf("3. Update Workout\n");
-		printf("4. Delete Workout\n");
-		printf("5. Search Workouts\n");
-		printf("6. Exit\n");
-		printf("Choose option: ");
+        switch (choice) {
+        case MENU_ADD:
+            addWorkout();
+            break;
+        case MENU_LIST:
+            listWorkouts();
+            break;
+        case MENU_UPDATE:
+            updateWorkout();
+            break;
+        case MENU_DELETE:
+            deleteWorkout();
+            break;
+        case MENU_SEARCH:
+            searchMenu();
+            break;
+        case MENU_EXIT:
+            saveToFile();
+            freeWorkouts();
+            printf("Thank you for using IronFlow!\n");
+            break;
+        default:
+            printf("Invalid input. Please try again.\n");
+            break;
+        }
 
-		int choice;
-		if (scanf("%d", &choice) != 1) {
-			while (getchar() != '\n');
-			printf("Invalid input\n");
-			continue;
-		}
-		while (getchar() != '\n');
-
-		switch (choice) {
-		case MENU_ADD:
-			addWorkout();
-			saveToFile();
-			break;
-		case MENU_LIST:
-			listWorkouts();
-			break;
-		case MENU_UPDATE:
-			updateWorkout();
-			saveToFile();
-			break;
-		case MENU_DELETE:
-			deleteWorkout();
-			saveToFile();
-			break;
-		case MENU_SEARCH:
-			searchMenu();
-			break;
-		case MENU_EXIT:
-			saveToFile();
-			freeWorkouts();
-			return;
-		default:
-			printf("Invalid option\n");
-		}
-	}
+    } while (choice != MENU_EXIT);
 }
